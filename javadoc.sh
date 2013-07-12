265b8dc459d2357e865c99eea0a00b38f95d1112
Pedro Lopes <pedro-lopes@premium-minds.com>
2013-07-12 16:08:07 +0100

    update javadoc.sh

--- javadoc.sh
+++ javadoc.sh
@@ -7,14 +7,15 @@ echo "creating a temporary directory"
 mkdir /tmp/DirAux
 
 echo "Generating javadoc for all projects"
+mvn install
 mvn javadoc:aggregate
 
-cp -r /target/site/apidocs/. /tmp/DirAux
+cp -r target/site/apidocs/. /tmp/DirAux
 mvn clean
 
 echo "changing to branch gh-pages"
 git checkout gh-pages
 
 echo "Copying Javadoc to Javadoc folder from the temporary folder"
-cp -r  /tmp/DirAux/. Javadoc/
+cp -r  tmp/DirAux/. Javadoc/
 
