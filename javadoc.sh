8f3ebdbe3bf24d4ba20e139ee9041efc773c003a
Pedro Lopes <pedro-lopes@premium-minds.com>
2013-07-12 15:44:19 +0100

    Add the javadoc script

---
+++ javadoc.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+echo "Changing to branch master"
+git checkout master
+
+echo "creating a temporary directory"
+mkdir /tmp/DirAux
+
+echo "Generating javadoc for all projects"
+mvn javadoc:aggregate
+cp /target/apidocs/. /tmp/DirAux
+mvn clean
+
+echo "changing to branch gh-pages"
+git checkout gh-pages
+
+echo "Copying Javadoc to Javadoc folder from the temporary folder"
+cp /tmp/DirAux/. Javadoc/ -r
+
+
