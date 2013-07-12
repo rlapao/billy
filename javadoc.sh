1c0c879fbfde8cf2b606b0448f6580983e642648
Pedro Lopes <pedro-lopes@premium-minds.com>
2013-07-12 15:49:21 +0100

    delete javadocs folders

--- javadoc.sh
+++ javadoc.sh
@@ -7,7 +7,7 @@ mkdir /tmp/DirAux
 
 echo "Generating javadoc for all projects"
 mvn javadoc:aggregate
-cp /target/apidocs/. /tmp/DirAux
+cp /target/site/apidocs/. /tmp/DirAux
 mvn clean
 
 echo "changing to branch gh-pages"
