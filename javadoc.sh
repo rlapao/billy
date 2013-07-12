562bd22776ebf4812d58dcc5fc8bc821e57ef0f0
Pedro Lopes <pedro-lopes@premium-minds.com>
2013-07-12 16:18:37 +0100

    Update javadoc.sh

--- javadoc.sh
+++ javadoc.sh
@@ -1,21 +1,28 @@
 #! /bin/bash
 
-echo "Changing to branch master"
+echo "Changing to branch master......"
 git checkout master
 
-echo "creating a temporary directory"
+echo "creating a temporary directory......."
 mkdir /tmp/DirAux
 
-echo "Generating javadoc for all projects"
+echo "Generating javadoc for all projects....."
 mvn install
 mvn javadoc:aggregate
 
 cp -r target/site/apidocs/. /tmp/DirAux
 mvn clean
 
-echo "changing to branch gh-pages"
+echo "changing to branch gh-pages....."
 git checkout gh-pages
 
-echo "Copying Javadoc to Javadoc folder from the temporary folder"
+echo "Copying Javadoc to Javadoc folder from the temporary folder....."
 cp -r  /tmp/DirAux/. Javadoc/
+
+echo "Starting to push the Javadoc to Git....."
+git add Javadoc
+git commit -a
+git push origin gh-pages
+
+echo "SUCESS the Javadoc of Billy is now updated!"
 
